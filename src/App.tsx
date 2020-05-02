444a4f45f31eb5a6e47fec370de7af933f0e1388
Rodrigo <rodrigo.hammes7@gmail.com>
2020-05-01 22:00:42 -0300

    Pasta context renomeada para hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider } from './hooks/AuthContext';
 
 import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
