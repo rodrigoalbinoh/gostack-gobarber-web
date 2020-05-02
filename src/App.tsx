6054ff83f09469c6863473c9bb9d126c8fef39d5
Rodrigo <rodrigo.hammes7@gmail.com>
2020-05-01 22:33:03 -0300

    Criado layout das Toast Messages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { AuthProvider } from './hooks/AuthContext';
 
-import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+
+import GlobalStyle from './styles/global';
+
+import ToastContainer from './components/ToastContainer';
 
 const App: React.FC = () => (
   <>
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+
+    <ToastContainer />
     <GlobalStyle />
   </>
 );
