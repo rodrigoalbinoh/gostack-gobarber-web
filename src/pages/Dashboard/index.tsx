238a5126ec7d76c7d3ac020930dccb8b71042621
Rodrigo <rodrigo.hammes7@gmail.com>
2020-07-17 23:39:56 -0300

    Criada seção principal do Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiPower, FiClock } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Section,
+  Appointment,
+  Calendar,
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   const { signOut, user } = useAuth();
 
   return (
@@ -28,6 +41,80 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars2.githubusercontent.com/u/1476845?s=460&u=c64deb14a46bd14c4938052c76335e48f7de14fd&v=4"
+                alt="Rodrigo Albino Hammes"
+              />
+              <strong>Rodrigo Albino Hammes</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+              <div>
+                <img
+                  src="https://avatars2.githubusercontent.com/u/1476845?s=460&u=c64deb14a46bd14c4938052c76335e48f7de14fd&v=4"
+                  alt="Rodrigo Albino Hammes"
+                />
+                <strong>Rodrigo Albino Hammes</strong>
+              </div>
+            </Appointment>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+              <div>
+                <img
+                  src="https://avatars2.githubusercontent.com/u/1476845?s=460&u=c64deb14a46bd14c4938052c76335e48f7de14fd&v=4"
+                  alt="Rodrigo Albino Hammes"
+                />
+                <strong>Rodrigo Albino Hammes</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+              <div>
+                <img
+                  src="https://avatars2.githubusercontent.com/u/1476845?s=460&u=c64deb14a46bd14c4938052c76335e48f7de14fd&v=4"
+                  alt="Rodrigo Albino Hammes"
+                />
+                <strong>Rodrigo Albino Hammes</strong>
+              </div>
+            </Appointment>
+          </Section>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
